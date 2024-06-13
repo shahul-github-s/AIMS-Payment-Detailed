f1ab27486ba7135772614501f3a62aec9c013af1
shahul-github-s <shahul.github@gmail.com>
2024-06-13 10:47:11 +0530

    update v1

--- script.js
+++ script.js
@@ -2,15 +2,27 @@ document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector("form");
 
   form.addEventListener("submit", function (e) {
+    const creditedTransactionMode = document.getElementById(
+      "credited-transaction-mode"
+    ).value;
+    const Dispositioned = document.getElementById("Dispositioned").value;
     e.preventDefault();
     const scriptURL =
       "https://script.google.com/macros/s/AKfycbykcwc23QKeY3AUfE8CBD6fN9ux504UVY33dIw-P2ngNgK5y2n-NasiOh86g6IfJDlZ/exec";
-    fetch(scriptURL, { method: "POST", body: new FormData(form) })
-      .then((response) => {
-        alert("Thank you! Your form is submitted successfully.");
-        console.log("Success!", response);
-      })
-      .catch((error) => console.error("Error!", error.message));
+    if (
+      creditedTransactionMode === "Select Transaction Mode" ||
+      Dispositioned === "Select Employeer Name"
+    ) {
+      alert("Please select a valid Transaction Details.");
+    } else {
+      fetch(scriptURL, { method: "POST", body: new FormData(form) })
+        .then((response) => {
+          alert("Thank you! Your form is submitted successfully.");
+          console.log("Success!", response);
+          location.reload();
+        })
+        .catch((error) => console.error("Error!", error.message));
+    }
   });
 
   const mobileNumberInput = document.getElementById("mobile-number");
